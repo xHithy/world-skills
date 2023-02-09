02b5f07156862fb2124a80aa7252067e96ee15ed
Hithy <55604744+xHithy@users.noreply.github.com>
2023-02-09 16:47:01 +0200

    Added user_id to token

--- database/migrations/2023_02_04_203606_create_token_table.php
+++ database/migrations/2023_02_04_203606_create_token_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('tokens', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
             $table->string('token');
             $table->string('expiring_at');
         });
